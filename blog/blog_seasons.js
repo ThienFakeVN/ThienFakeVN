812676732618d00d3fc8f83c9426cd6e5ca411ae
ThienFakeVN <ngotranquangthien2011@gmail.com>
2025-07-30 19:15:09 +0700

    Update blog_seasons.js

--- blog/blog_seasons.js
+++ blog/blog_seasons.js
@@ -13,17 +13,17 @@ const currentSeason = seasons(today);
 console.log(currentSeason)
 if (currentSeason === "Spring" || currentSeason === "Reunification Day") {
     document.getElementById("season").textContent = "body {background-image: url('https://upload.wikimedia.org/wikipedia/commons/f/fb/XN_Fruehjahrswiese_00.jpg');}";
-    document.getElementById("credit").innerText = "Credit for the background: Original: Guido Gerding / Wikimedia Commons / “XN Fruehjahrswiese 00” / CC BY-SA 3.0";
+    document.getElementById("credit").innerText = "[Credit for the background] Original: Guido Gerding / Wikimedia Commons / “XN Fruehjahrswiese 00” / CC BY-SA 3.0";
     } // Original: Guido Gerding / Wikimedia Commons / “XN Fruehjahrswiese 00” / CC BY-SA 3.0
 else if (currentSeason === "Summer") {
     document.getElementById("season").textContent = "body {background-image: url('https://upload.wikimedia.org/wikipedia/commons/c/c4/Field_Hamois_Belgium_Luc_Viatour.jpg');}";
-    document.getElementById("credit").innerText = "Credit for the background: Original: Luc Viatour | https://Lucnix.be / Wikimedia Commons / “Field Hamois Belgium Luc Viatour” / CC BY-SA 3.0";
+    document.getElementById("credit").innerText = "[Credit for the background] Original: Luc Viatour | https://Lucnix.be / Wikimedia Commons / “Field Hamois Belgium Luc Viatour” / CC BY-SA 3.0";
     } // Original: Luc Viatour | https://Lucnix.be / Wikimedia Commons / “Field Hamois Belgium Luc Viatour” / CC BY-SA 3.0
 else if (currentSeason === "Autumn" || currentSeason === "Independence Day" || currentSeason === "My birthday!") {
     document.getElementById("season").textContent = "body {background-image: url('https://upload.wikimedia.org/wikipedia/commons/d/d0/D%C3%BClmen%2C_Wildpark_--_2014_--_3808_color_balanced.jpg');}";
-    document.getElementById("credit").innerText = "Credit for the background: Original: Dietmar Rabich (Derivative work: Sting) / Wikimedia Commons / “Dülmen, Wildpark -- 2014 -- 3808 color balanced” / CC BY-SA 4.0;"
+    document.getElementById("credit").innerText = "[Credit for the background] Original: Dietmar Rabich (Derivative work: Sting) / Wikimedia Commons / “Dülmen, Wildpark -- 2014 -- 3808 color balanced” / CC BY-SA 4.0;"
     } // Original: Dietmar Rabich (Derivative work: Sting) / Wikimedia Commons / “Dülmen, Wildpark -- 2014 -- 3808 color balanced” / CC BY-SA 4.0
 else {
     document.getElementById("season").textContent = "body {background-image: url('https://upload.wikimedia.org/wikipedia/commons/b/b0/Winter_forest_silver.jpg');}";
-    document.getElementById("credit").innerText = "Credit for the background: Original: Ernst Vikne / Wikipedia Commons / “Winter forest silver” / CC BY-SA 2.0";
+    document.getElementById("credit").innerText = "[Credit for the background] Original: Ernst Vikne / Wikipedia Commons / “Winter forest silver” / CC BY-SA 2.0";
     } // Original: Ernst Vikne / Wikipedia Commons / “Winter forest silver” / CC BY-SA 2.0
